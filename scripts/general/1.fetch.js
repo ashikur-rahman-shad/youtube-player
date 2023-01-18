6731d574ffca7bc684db07df21f363d768b5b5e5
Ashikur Rahman Shad <shadgood@gmail.com>
2023-01-18 22:18:20 +0600

    fetch api script error fixed

--- scripts/general/1.fetch.js
+++ scripts/general/1.fetch.js
@@ -25,7 +25,6 @@ for(i=0;i<elements.length;i++){
         fetchContent(elements[i],file);
 }
 
-d
 //writen by Ashikur Rahman Shad 
 //I took some help from the link below 
 //https://www.w3schools.com/howto/howto_html_include.asp 
