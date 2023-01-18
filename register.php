aaf93eb632658764b7a47590cf8666170e943467
Ashikur Rahman Shad <shadgood@gmail.com>
2023-01-18 22:43:28 +0600

    file renamed

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
     require 'backend/database/db-connect.php';
 
-    require 'backend/register/1.global.php';
+    require 'backend/register/1.declarations.php';
     require 'backend/register/2.validation.php';
     require 'backend/register/3.print-errors.php';
     require 'backend/register/4.add-database.php';
