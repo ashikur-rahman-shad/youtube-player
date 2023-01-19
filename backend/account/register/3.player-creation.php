4c60194c3b2b5577f0633fbd2b62c285eff2117c
Ashikur Rahman Shad <shadgood@gmail.com>
2023-01-19 18:55:37 +0600

    sql refactoring for player creation

--- backend/account/register/3.player-creation.php
+++ backend/account/register/3.player-creation.php
@@ -3,8 +3,8 @@ if (isset($_POST["reg"]) && count($errorMessages) == 0) {
 
     $stream_key = $user_id . date('YmdHis');
 
-    $sql = "INSERT INTO `players` (`streamkey`, `owner`, `incident`, `vid_id`, `duration`, `state`)VALUES 
-    ('" . $stream_key . "', '" . $user_id . "', '" . date('Y-m-d H:i:s') . "', 'PEwJ9VIlNgM', '0', '1')";
+    $sql = "INSERT INTO `players` (`streamkey`, `owner`)VALUES 
+    ('" . $stream_key . "', '" . $user_id . "')";
 
     sql($sql);
 }
