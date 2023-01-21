45c1b50dc6c67daa34f7a2af10694bd2fa914223
Ashikur Rahman Shad <shadgood@gmail.com>
2023-01-21 11:03:33 +0600

    support added for youtu.be links

--- scripts/ytPlayer/4.videoURL.js
+++ scripts/ytPlayer/4.videoURL.js
@@ -1,11 +1,23 @@
 function changeURL() {
+
     url = document.getElementsByName("url")[0].value;
-    if (url.indexOf("v=")>-1)
-        url = url.slice(url.indexOf("v=") + 2);
-    if (url.indexOf("&")>-1)
-        url = url.slice(0, url.indexOf("&"));
-    player.loadVideoById(url);
+
+    if (url.includes(".com")) {
+
+        if (url.indexOf("v=") > -1)
+            url = url.slice(url.indexOf("v=") + 2);
+        if (url.indexOf("&") > -1)
+            url = url.slice(0, url.indexOf("&"));
+        player.loadVideoById(url);
+    }
+
+    else if (url.includes(".be")) {
+
+        if (url.indexOf("v=") > -1)
+            url = url.slice(url.indexOf(".be") + 3);
+    }
+
     //console.log(url)
-    update("vidid="+url+"&duration=0");
+    update("vidid=" + url + "&duration=0");
 }
 //https://www.youtube.com/watch?v=mATzn3YfnUQ
